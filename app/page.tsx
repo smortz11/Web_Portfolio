a1ee6179e6ba31b49fe8262a092d4693a73bd118
smortz11 <a.swartz.cs@protonmail.com>
2026-09-25 14:33:20 +0000

    Update NSE4 progress to 63% (10 of 16 lessons)

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ const certifications = [
   { label: "Cisco Certified Network Associate (CCNA)", value: 100, subtitle: "Earned Aug 2026" },
   { label: "CompTIA Security+", value: 100, subtitle: "Earned Jun 2025" },
   { label: "CompTIA CySA+", value: 100, subtitle: "Earned Sep 2025" },
-  { label: "Fortinet NSE 4", value: 57, subtitle: "In Progress" },
+  { label: "Fortinet NSE 4", value: 63, subtitle: "In Progress" },
 ]
 
 function Divider() {
